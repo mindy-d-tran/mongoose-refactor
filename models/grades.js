9ffa9f9ba1abe10a4985d3564fde3f26287385be
Mindy D Tran <mindytran98@gmail.com>
2024-01-18 11:37:59 -0500

    create schema for grades

---
+++ models/grades.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const gradesSchema = new mongoose.Schema({
+    scores: [{type: String, score: Number}]
+    ,
+    class_id: {
+        type: Number,
+        required: true
+    },
+    learner_id: {
+        type: Number,
+        required: true
+    }
+})
+
+export default mongoose.model('Grade', gradesSchema)
