82bb3ae72c63f7cf6b062c6cb05fa5fab957337b
Mindy D Tran <mindytran98@gmail.com>
2024-01-18 11:43:39 -0500

    refactoring code to mongoose

--- db/conn.js
+++ db/conn.js
@@ -1,18 +1,28 @@
-import {MongoClient} from 'mongodb';
+// import {MongoClient} from 'mongodb';
+import mongoose from "mongoose";
 
-const connectionString = process.env.ATLAS_URI || "";
-console.log(process.env.ATLAS_URI);
-
-const client = new MongoClient(connectionString);
-
-let conn;
-
-try {
-    conn = await client.connect();
-    console.log("connnected to mongodb");
-} catch (e) {
-    console.log(e);
+export async function conn(){
+    try {
+        await mongoose.connect(process.env.ATLAS_URI);
+        console.log('connected to MongoDB');
+    } catch (error) {
+        console.log(error.message);
+    }
 }
 
-const db = conn.db("sample_training");
-export default db;
+// const connectionString = process.env.ATLAS_URI || "";
+// console.log(process.env.ATLAS_URI);
+
+// const client = new MongoClient(connectionString);
+
+// let conn;
+
+// try {
+    //     conn = await client.connect();
+    //     console.log("connnected to mongodb");
+    // } catch (e) {
+        //     console.log(e);
+        // }
+        
+        // const db = conn.db("sample_training");
+        // export default db;
